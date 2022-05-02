dd01c5ee82daa0db3641d6cb62cb2a58fba060a7
Danielyepes <danielyepesahumedo@hotmail.com>
2022-05-02 15:35:24 -0500

    creating project and config

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+   
+  { 
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then( m => m.AuthModule )
+  },
+  { 
+    path: 'product',
+    loadChildren: () => import('./product/product.module').then( m => m.ProductModule )
+  },
+
+  {
+    path: '**',
+    redirectTo: 'auth'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
